90ac8f6e0b507f2d9d02bcf862aba7655ed7c7c6
Jimmy Sung <jimmy43333@gmail.com>
2018-04-11 10:04:08 +0800

    Initial Commit

---
+++ Robot_OpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Robot_OpenGL
+//
+//  Created by TG-SUNG on 2018/4/11.
+//  Copyright © 2018年 宋題均. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
